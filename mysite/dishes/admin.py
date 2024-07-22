3109a4280820dd18191584079dc8b15d8c869f01
Fofone9 <mr.paa2004@gmail.com>
2024-07-22 12:26:09 +0300

    изменил блюда

--- mysite/dishes/admin.py
+++ mysite/dishes/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from rest_framework.authtoken.admin import TokenAdmin
-from .models import Dish, Ingredient, Composition, Cook
+from .models import Dish, Ingredient, Cook
 # Register your models here.
 admin.site.register(Dish)
 admin.site.register(Ingredient)
-admin.site.register(Composition)
+# admin.site.register(Composition)
 admin.site.register(Cook)
 TokenAdmin.raw_id_fields = ['user']
--- mysite/dishes/models.py
+++ mysite/dishes/models.py
@@ -25,24 +25,9 @@ class Cook(models.Model):
 class Dish(models.Model):
     name = models.TextField(max_length=50)
     author = models.ForeignKey('Cook', on_delete=models.CASCADE )
-    DISH_TYPES = (
-        ('Bakery', 'Выпечка'),
-        ('Breakfast', 'Завтрак'),
-        ('Dessert', 'Десерт'),
-        ('On coals', 'На углях'),
-        ('Beverages', 'Напитки'),
-        ('Soups', 'Супы'),
-        ('Cold snacks','Холодные закуски'),
-        ('Hot dishes', 'Горячие блюда'),
-        ('Side dishes', 'Гарниры'),
-    )
-    dish_type = models.CharField(max_length=40, choices=DISH_TYPES)
-    ingredients = models.ManyToManyField(
-        'Ingredient',
-        through='Composition',
-        through_fields=('dish','ingredient'),
-    )
-
+    dish_type = models.CharField(max_length=40)
+    ingredients = models.ManyToManyField('Ingredient')
+    price = models.IntegerField()
     def __str__(self):
         return str(self.name)
 
@@ -54,9 +39,9 @@ class Ingredient(models.Model):
     def __str__(self):
         return str(self.name)
 
-class Composition(models.Model):
-    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    count = models.SmallIntegerField()
+# class Composition(models.Model):
+#     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+#     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+#     count = models.SmallIntegerField()
 
     
